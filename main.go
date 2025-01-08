093392047d5cf113526717e78b104828948f11fc
shubhra tiwary <shubhratiwary5@gmail.com>
2025-01-09 04:00:55 +0530

    variable use

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var username = "shubhra "
-	fmt.Print("hello ", username, "world")
+	var nameOfConference = "Go conference"
+	fmt.Println("welcome this is ", nameOfConference)
+	fmt.Print("Welcome to the booking app")
 }
