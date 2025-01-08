79d0fb83539a68b66d0e54769303d7fb1178980a
shubhra tiwary <shubhratiwary5@gmail.com>
2025-01-09 04:29:50 +0530

    %T gives the dataType of the variable and uint is the dataType that has integer values but not negative integers

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var nameOfConference = "Go conference"
+	var nameOfConference string = "Go conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
 	fmt.Printf("welcome this is %v \n", nameOfConference)
 	fmt.Printf("Welcome to the booking app, the tickets available are %v and the total tickets are %v \n", remainingTickets, conferenceTickets)
-
+	fmt.Printf("remaining ticket is of %T type \n", remainingTickets)
 	var username string
 	var userTickets int
 	username = "tom"
