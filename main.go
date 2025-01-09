643f243e10e3227ce7f65278666e7d843604ca41
shubhra tiwary <shubhratiwary5@gmail.com>
2025-01-10 03:41:05 +0530

    slices and array

--- main.go
+++ main.go
@@ -8,18 +8,25 @@ func main() {
 	var remainingTickets uint = 50
 	fmt.Printf("welcome this is %v \n", nameOfConference)
 	fmt.Printf("Welcome to the booking app, the tickets available are %v and the total tickets are %v \n", remainingTickets, conferenceTickets)
-	fmt.Printf("remaining ticket is of %T type \n", remainingTickets)
+	//Array
+	//var bookings [50]string
+
+	//slice
+	var bookings []string
 
 	//userTicket logic
 	var username string
-	var userTickets int
+	var userTickets uint
 	var userEmail string
 	fmt.Printf("enter your name \n")
-	fmt.Scan(&username)
+	fmt.Scan(&username) // for taking the input you provide the pointer for the variable so that the scan function where to store the value inputted
+	bookings = append(bookings, username)
 	fmt.Printf("enter the tickets \n")
 	fmt.Scan(&userTickets)
+	remainingTickets = remainingTickets - userTickets
 	fmt.Printf("enter the email of the user \n")
 	fmt.Scan(&userEmail)
-
-	fmt.Printf("user named %v booked %v tickets \n, you will be sent confirmation of the ticket at %v \n", username, userTickets, userEmail)
+	fmt.Printf("user named %v booked %v tickets , you will be sent confirmation of the ticket at %v \n", username, userTickets, userEmail)
+	fmt.Printf("remaining tickets are %v \n", remainingTickets)
+	fmt.Printf("the booking is %v and the total people who booked are %v", bookings, len(bookings))
 }
